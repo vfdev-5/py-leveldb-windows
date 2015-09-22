65145a0652841b1bb34201427a02a9d1d5ad5773
vfomin <vfomin@FRTOUPC062.telespazio.corp>
2015-09-22 11:00:50 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Python leveldb wrapper for Caffe & Caffe-builder under Windows
+Helper project to compile Python leveldb wrapper under Windows using CMake.
+
+C++ libraries (leveldb and its dependencies) should be taken from Caffe-builder's cache
+
+## Usage
+1. Setup the configuration in the build_leveldb.bat
+E.g. set CAFFE_PATH=C:/Caffe-willyd/install 
+and optionally
+set build_mode=Release
+set source_dir_name=py-leveldb-windows
+set build_dir_name=%source_dir_name%-build
+set install_dir_path=../%source_dir_name%-install
+
+2. Run build_leveldb.bat
+
+3. Copy `%install_dir_path%/leveldb.pyd` and `%install_dir_path%/leveldb.lib` to `%PYTHON_PATH%/Lib/site-packages`
+
+4. Run `python ./test-py-leveldb.py`. If everything is ok, you will get `hello world` response and a folder named `db`.
+(Test is taken from https://github.com/happynear/py-leveldb-windows)
---
+++ build_leveldb.bat
@@ -0,0 +1,64 @@
+@echo off
+
+:: ***********************************************************************
+:: Build Environment & CMAKE configuration
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
+set CMAKE="%PROGRAMFILES(x86)%\CMake\bin\cmake.exe"
+set CMAKE_GENERATOR="CodeBlocks - NMake Makefiles"
+
+:: ***********************************************************************
+:: Project dependencies configuration
+
+:: Path to the Caffe installation
+set CAFFE_PATH=C:/VFomin_folder/Caffe-willyd/Caffe-release
+
+:: ***********************************************************************
+:: Project names
+
+set build_mode=Release
+set source_dir_name=py-leveldb-windows
+set build_dir_name=%source_dir_name%-build
+set install_dir_path=../%source_dir_name%-install
+
+:: ***********************************************************************
+:: ***********************************************************************
+:: ***********************************************************************
+
+cd ..
+if not exist build_dir_name (
+	mkdir %build_dir_name%
+)
+cd %build_dir_name%
+
+:: LevelDB depends on Boost under Windows
+set LEVELDB_INCLUDE_DIR=%CAFFE_PATH%/include
+set LEVELDB_LIBRARY_DIR=%CAFFE_PATH%/lib
+set LEVELDB_LIBRARY=^
+leveldb.lib;^
+libboost_thread-vc120-mt-1_58.lib;^
+libboost_system-vc120-mt-1_58.lib;^
+libboost_regex-vc120-mt-1_58.lib;^
+libboost_python-vc120-mt-1_58.lib;^
+libboost_filesystem-vc120-mt-1_58.lib;^
+libboost_date_time-vc120-mt-1_58.lib;^
+libboost_chrono-vc120-mt-1_58.lib
+
+set CMAKE_PARAMETERS=^
+-DCMAKE_INSTALL_PREFIX=%install_dir_path% ^
+-DCMAKE_BUILD_TYPE=%build_mode%
+
+:: ----------------------------------------------------
+%CMAKE% %CMAKE_PARAMETERS% -G %CMAKE_GENERATOR% ..\%source_dir_name%
+%CMAKE% --build .
+nmake install
+
+PAUSE
+
+
+
+
+
+
+
+
